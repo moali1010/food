cab01a89d3afd470108c3222c86230dbf5bae2da
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 09:10:32 +0330

    Create InvalidDiscountCode.java

---
+++ src/main/java/Exceptions/InvalidDiscountCode.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class InvalidDiscountCode extends RuntimeException {
+    public InvalidDiscountCode(String message) {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
