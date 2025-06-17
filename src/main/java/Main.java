9fbe75fbb83ca89155aca3be6a0af9e3ba6a841c
moali1010 <moali.develop@gmail.com>
2025-06-17 10:13:32 +0330

    test commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import JavaFood.AdminPanel;
 public class Main {
     public static void main(String[] args) {
         AdminPanel adminPanel = new AdminPanel();
-        adminPanel.loadFromJSONFile("data2.json");
+//        adminPanel.loadFromJSONFile("data2.json");
         // RestaurantIsClose Exception -> Restaurant is closed
     }
 }
