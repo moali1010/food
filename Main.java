5840f1d0edbdcde324c588dbd6eb488904c9e130
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 08:52:44 +0330

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import JavaFood.AdminPanel;
-
-public class Main {
-    public static void main(String[] args) {
-        AdminPanel adminPanel = new AdminPanel();
-        adminPanel.loadFromJSONFile("data2.json");
-        // RestaurantIsClose Exception -> Restaurant is closed
-    }
-}
