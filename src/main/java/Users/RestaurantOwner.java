4239287c77c10acedb098a8e479ae33cbc9508a5
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 09:21:25 +0330

    Create RestaurantOwner

---
+++ src/main/java/Users/RestaurantOwner.java
@@ -0,0 +1,11 @@
+package Users;
+import Models.Restaurant;
+
+import java.util.ArrayList;
+
+public class RestaurantOwner extends User {
+
+    public RestaurantOwner(int id, String name) {
+        super(id, name);
+    }
+}
