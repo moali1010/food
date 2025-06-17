11ebf86d8547274a88efd1efadc258e98de37927
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 09:22:07 +0330

    Create User

---
+++ src/main/java/Users/User.java
@@ -0,0 +1,19 @@
+package Users;
+import JavaFood.AdminPanel;
+import Models.Order;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class User {
+    private int id;
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ArrayList<Order> getOrders() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
