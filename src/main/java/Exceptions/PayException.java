2b9a459c5ace9622c60d94581b25832c3125f901
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 09:11:58 +0330

    Create PayException.java

---
+++ src/main/java/Exceptions/PayException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class PayException extends RuntimeException {
+    public PayException() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
