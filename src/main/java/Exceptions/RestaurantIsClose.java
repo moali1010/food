e3fffbacfa216237f5ae7471afdac36f577e12b1
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 09:12:39 +0330

    Create RestaurantIsClose

---
+++ src/main/java/Exceptions/RestaurantIsClose.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class RestaurantIsClose extends RuntimeException {
+    public RestaurantIsClose() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
