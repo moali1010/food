675d1a266f8e80eb6ee200213509a66178e6283a
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 08:47:27 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import JavaFood.AdminPanel;
+
+public class Main {
+    public static void main(String[] args) {
+        AdminPanel adminPanel = new AdminPanel();
+        adminPanel.loadFromJSONFile("data2.json");
+        // RestaurantIsClose Exception -> Restaurant is closed
+    }
+}
