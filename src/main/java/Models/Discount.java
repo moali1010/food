8ae8e8798c2a672674440cab62db33af32454044
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 09:15:16 +0330

    Create Discount

---
+++ src/main/java/Models/Discount.java
@@ -0,0 +1,30 @@
+package Models;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Discount {
+    private int id;
+    private String code;
+    private Integer userId;
+    private DiscountType discountType;
+    private LocalDate expireDate;
+    private Double amount;
+    private Integer percentage;
+    private Boolean isUsed = false;
+
+    public enum DiscountType {
+        PERCENTAGE,
+        AMOUNT
+    }
+
+    public Discount(Integer id, String code, Double amount, LocalDate expireDate, Integer userId) {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    public Discount(Integer id, String code, Integer percentage, LocalDate expireDate, Integer userId) {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
