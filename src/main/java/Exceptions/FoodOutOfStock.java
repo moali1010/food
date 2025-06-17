a5e322704894fb1cd9e6a5b64cf30a7a6c4aa9a2
Mohammad Ali Ebrahimi <moali.develop@gmail.com>
2025-06-17 09:07:40 +0330

    Create FoodOutOfStock

---
+++ src/main/java/Exceptions/FoodOutOfStock.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class FoodOutOfStock extends RuntimeException {
+    public FoodOutOfStock() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
