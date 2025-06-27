1354cef3ca159719303bde52e8f961b71a68f7ff
moali1010 <moali.develop@gmail.com>
2025-06-27 20:52:20 +0330

    Phase3: AdminPanel

--- src/main/java/JavaFood/AdminPanel.java
+++ src/main/java/JavaFood/AdminPanel.java
@@ -22,34 +22,139 @@ public class AdminPanel {
     public static ArrayList<Order> orders = new ArrayList<>();
 
     public void loadFromJSONFile(String fileAddress) {
-        throw new UnsupportedOperationException("not implemented yet");
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(fileAddress)) {
+            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+
+            // read today date
+            setDate(LocalDate.parse(jsonObject.get("todayDate").getAsString()));
+
+            // create Restaurants
+            JsonArray restaurants = jsonObject.getAsJsonArray("restaurants");
+            for (JsonElement restaurant : restaurants) {
+                JsonObject restObj = restaurant.getAsJsonObject();
+                Restaurant r = new Restaurant(restObj.get("id").getAsInt(), restObj.get("name").getAsString(), restObj.get("address").getAsString(),
+                        restObj.get("openHour").getAsInt(), restObj.get("closeHour").getAsInt(),
+                        Restaurant.RestaurantTypes.valueOf(restObj.get("type").getAsString()));
+
+                if (restObj.has("isOpen") && restObj.get("isOpen").getAsBoolean()) {
+                    r.openRestaurant();
+                }
+
+                JsonArray foods = restObj.getAsJsonArray("foods");
+                for (JsonElement food : foods) {
+                    JsonObject foodObj = food.getAsJsonObject();
+                    Food f = new Food(foodObj.get("id").getAsInt(), foodObj.get("name").getAsString(), foodObj.get("price").getAsDouble());
+                    r.addFood(f, foodObj.get("quantity").getAsInt());
+                }
+
+                addRestaurant(r);
+            }
+
+            // access Users
+            JsonObject users = jsonObject.getAsJsonObject("users");
+            JsonArray restaurantOwners = users.getAsJsonArray("restaurantOwners");
+            JsonArray admins = users.getAsJsonArray("admins");
+            JsonArray customers = users.getAsJsonArray("customers");
+
+            // add restaurant Owners
+            for (JsonElement owner : restaurantOwners) {
+                JsonObject rOObj = owner.getAsJsonObject();
+                RestaurantOwner ro = new RestaurantOwner(rOObj.get("id").getAsInt(), rOObj.get("name").getAsString());
+                addUser(ro);
+            }
+
+            // add admins
+            for (JsonElement admin : admins) {
+                JsonObject adminObj = admin.getAsJsonObject();
+                RestaurantOwner a = new RestaurantOwner(adminObj.get("id").getAsInt(), adminObj.get("name").getAsString());
+                addUser(a);
+            }
+
+            // add customers
+            for (JsonElement customer : customers) {
+                JsonObject customerObj = customer.getAsJsonObject();
+                RestaurantOwner c = new RestaurantOwner(customerObj.get("id").getAsInt(), customerObj.get("name").getAsString());
+                addUser(c);
+            }
+
+            // add discounts
+            JsonArray discounts = jsonObject.getAsJsonArray("discounts");
+            for (JsonElement discount : discounts) {
+                JsonObject discObj = discount.getAsJsonObject();
+                if (discObj.has("amount")) {
+                    Discount d = new Discount(discObj.get("id").getAsInt(), discObj.get("code").getAsString(),
+                            discObj.get("amount").getAsDouble(), LocalDate.parse(discObj.get("expireDate").getAsString()), discObj.get("userId").getAsInt());
+                    addDiscount(d);
+                } else {
+                    Discount d = new Discount(discObj.get("id").getAsInt(), discObj.get("code").getAsString(),
+                            discObj.get("percentage").getAsInt(), LocalDate.parse(discObj.get("expireDate").getAsString()), discObj.get("userId").getAsInt());
+                    addDiscount(d);
+                }
+            }
+
+            // add orders
+            JsonArray orders = jsonObject.getAsJsonArray("orders");
+            for (JsonElement order : orders) {
+                JsonObject orderObj = order.getAsJsonObject();
+                Order o = createOrder(orderObj.get("id").getAsInt(), orderObj.get("userId").getAsInt(), orderObj.get("restaurantId").getAsInt(), Order.ReceivingType.valueOf(orderObj.get("receivingType").getAsString()));
+
+                // add foods to order
+                JsonArray foods = orderObj.getAsJsonArray("foods");
+                for (JsonElement food : foods) {
+                    JsonObject foodObj = food.getAsJsonObject();
+                    o.addFoodToOrder(foodObj.get("foodId").getAsInt(), foodObj.get("quantity").getAsInt());
+                }
+
+                // add discount codes
+                if (orderObj.has("discountCodes")) {
+                    JsonArray discountCodes = orderObj.getAsJsonArray("discountCodes");
+                    for (JsonElement discountCode : discountCodes) {
+                        o.addDiscount(discountCode.getAsString());
+                    }
+                }
+
+                // pay order
+                if (orderObj.has("isPaid") && orderObj.get("isPaid").getAsBoolean()) {
+                    o.pay(orderObj.get("payAmount").getAsDouble());
+                }
+
+                // score order
+                if (orderObj.has("score")) {
+                    o.scoreOrder(orderObj.get("score").getAsInt());
+                }
+            }
+
+        } catch (IOException | JsonSyntaxException e) {
+//            e.printStackTrace();
+        }
     }
 
     public void addDiscount(Discount discount) {
-        throw new UnsupportedOperationException("not implemented yet");
+        discounts.add(discount);
     }
 
     public void addUser(User user) {
-        throw new UnsupportedOperationException("not implemented yet");
+        users.add(user);
     }
 
     public void addRestaurant(Restaurant restaurant) {
-        throw new UnsupportedOperationException("not implemented yet");
+        restaurants.add(restaurant);
     }
 
     public Order createOrder(Integer id, Integer userId, Integer restaurant_id, Order.ReceivingType type) {
-        throw new UnsupportedOperationException("not implemented yet");
+        return new Order(id, userId, restaurants.stream().filter(r -> Objects.equals(r.getId(), restaurant_id)).findFirst().orElse(null), type);
     }
 
     public void setDate(LocalDate todayDate) {
-        throw new UnsupportedOperationException("not implemented yet");
+        AdminPanel.todayDate = todayDate;
     }
 
     public Restaurant getBestRestaurant() {
-        throw new UnsupportedOperationException("not implemented yet");
+        return restaurants.get(0);
     }
 
     public Restaurant getMostOrderedRestaurant() {
-        throw new UnsupportedOperationException("not implemented yet");
+        return restaurants.get(0);
     }
 }
